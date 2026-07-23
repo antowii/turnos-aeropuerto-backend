45bebfb0394c98780dad97f0f827f0436db0f351
Antonella <antonellanattino@hotmail.com>
2026-07-22 22:51:32 -0400

    Agregadas entidades Tienda y Asistencia con relaciones

---
+++ src/main/java/com/aeropuerto/turnos/model/Asistencia.java
@@ -0,0 +1,21 @@
+package com.aeropuerto.turnos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Asistencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //Se conectan las dos tablas
+    @ManyToOne
+    @JoinColumn(name = "trabajador_id")
+    private Trabajador trabajador;
+
+    private String motivo; // Ej: "Vacaciones", "Licencia", "Cumpleaños"
+    private LocalDate fecha;
+}
---
+++ src/main/java/com/aeropuerto/turnos/model/Tienda.java
@@ -0,0 +1,20 @@
+package com.aeropuerto.turnos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Tienda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String terminal;
+    private int capacidad;
+    private boolean requiereTica;
+}
