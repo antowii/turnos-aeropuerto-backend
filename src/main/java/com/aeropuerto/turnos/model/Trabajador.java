5afdd0c1177cd60e4252803bbd2cb53cf04e417b
Antonella <antonellanattino@hotmail.com>
2026-07-22 20:43:18 -0400

    Primer commit: Estructura base de Spring Boot y clase Trabajador

---
+++ src/main/java/com/aeropuerto/turnos/model/Trabajador.java
@@ -0,0 +1,20 @@
+package com.aeropuerto.turnos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Trabajador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombreCompleto;
+    private String cargo;
+    private boolean tieneTica;
+    private String ultimaTiendaTrabajada;
+}
